1572dcd2d19ccc06205f13e256ae64bae4fd8ded
Ned Zimmerman <ned@bight.dev>
2024-04-08 09:44:46 -0300

    chore(deps-dev): remove React plugins

--- .xo.config.cjs
+++ .xo.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["xo-react", "plugin:yml/standard", "plugin:markdown/recommended"],
+  extends: ["plugin:yml/standard", "plugin:markdown/recommended"],
   ignorePatterns: ["_site/", "!.*.js", "!.github/"],
   overrides: [
     {
