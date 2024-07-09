667e8b1ab928615292026ecae05a2232e8efbae1
Ned Zimmerman <ned@bight.dev>
2024-07-09 10:36:50 -0300

    test: add Ava and write some tests (#94)

---
+++ test/matching.test.js
@@ -0,0 +1,27 @@
+import test from "ava";
+import matchesLevel from "../src/_utils/matches-level.js";
+import matchesVersion from "../src/_utils/matches-version.js";
+
+test("target levels match expectations", (t) => {
+    t.true(matchesLevel("A", "A"));
+    t.true(matchesLevel("A", "AA"));
+    t.true(matchesLevel("A", "AAA"));
+    t.false(matchesLevel("AA", "A"));
+    t.true(matchesLevel("AA", "AA"));
+    t.true(matchesLevel("AA", "AAA"));
+    t.false(matchesLevel("AAA", "A"));
+    t.false(matchesLevel("AAA", "AA"));
+    t.true(matchesLevel("AAA", "AAA"));
+});
+
+test("target versions match expectations", (t) => {
+    t.true(matchesVersion(["2.0", "2.1", "2.2"], "2.0"));
+    t.true(matchesVersion(["2.0", "2.1", "2.2"], "2.1"));
+    t.true(matchesVersion(["2.0", "2.1", "2.2"], "2.2"));
+    t.false(matchesVersion(["2.1", "2.2"], "2.0"));
+    t.true(matchesVersion(["2.1", "2.2"], "2.1"));
+    t.true(matchesVersion(["2.1", "2.2"], "2.2"));
+    t.false(matchesVersion(["2.2"], "2.0"));
+    t.false(matchesVersion(["2.2"], "2.1"));
+    t.true(matchesVersion(["2.2"], "2.2"));
+});
