fb534b0452aebbf36ae7eeec7aba005c8df079a7
Ned Zimmerman <ned@bight.dev>
2024-07-09 09:52:18 -0300

    chore: switch from XO to Biome with biome-config-fluid (#93)

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: ["stylelint-config-xo"],
-  plugins: ["stylelint-order"],
-  ignoreFiles: ["_site/**/*.css"],
-  rules: {
-    "custom-property-pattern": null,
-    "import-notation": "string",
-    "selector-class-pattern": null,
-    "order/properties-alphabetical-order": true,
-    "selector-type-no-unknown": [true, { ignore: ["custom-elements"] }],
-  },
+    extends: ["stylelint-config-xo"],
+    plugins: ["stylelint-order"],
+    ignoreFiles: ["_site/**/*.css"],
+    rules: {
+        "custom-property-pattern": null,
+        "import-notation": "string",
+        "selector-class-pattern": null,
+        "order/properties-alphabetical-order": true,
+        "selector-type-no-unknown": [true, { ignore: ["custom-elements"] }]
+    }
 };
--- src/_data/totalsbylevel.js
+++ src/_data/totalsbylevel.js
@@ -1,105 +1,105 @@
-import successCriteria from './successcriteria.js';
+import successCriteria from "./successcriteria.js";
 
 export default async function totalsByLevel() {
-	const totals = {
-		2.2: {
-			A: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AAA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-		},
-		2.1: {
-			A: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AAA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-		},
-		'2.0': {
-			A: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-			AAA: {
-				all: 0,
-				perceivable: 0,
-				operable: 0,
-				understandable: 0,
-				robust: 0,
-			},
-		},
-	};
-	let criteria;
+    const totals = {
+        2.2: {
+            A: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AAA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            }
+        },
+        2.1: {
+            A: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AAA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            }
+        },
+        "2.0": {
+            A: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            },
+            AAA: {
+                all: 0,
+                perceivable: 0,
+                operable: 0,
+                understandable: 0,
+                robust: 0
+            }
+        }
+    };
+    let criteria;
 
-	try {
-		criteria = await successCriteria();
-	} catch (error) {
-		console.error(`Fetch failed in totalsPerLevel.js. ${error}`);
-	}
+    try {
+        criteria = await successCriteria();
+    } catch (error) {
+        console.error(`Fetch failed in totalsPerLevel.js. ${error}`);
+    }
 
-	for (const sc of Object.values(criteria)) {
-		for (const version of sc.versions) {
-			if (sc.level !== '') {
-				totals[version][sc.level].all++;
-				totals[version][sc.level][sc.principle.toLowerCase()]++;
+    for (const sc of Object.values(criteria)) {
+        for (const version of sc.versions) {
+            if (sc.level !== "") {
+                totals[version][sc.level].all++;
+                totals[version][sc.level][sc.principle.toLowerCase()]++;
 
-				if (sc.level === 'A') {
-					totals[version].AA.all++;
-					totals[version].AA[sc.principle.toLowerCase()]++;
-					totals[version].AAA.all++;
-					totals[version].AAA[sc.principle.toLowerCase()]++;
-				}
+                if (sc.level === "A") {
+                    totals[version].AA.all++;
+                    totals[version].AA[sc.principle.toLowerCase()]++;
+                    totals[version].AAA.all++;
+                    totals[version].AAA[sc.principle.toLowerCase()]++;
+                }
 
-				if (sc.level === 'AA') {
-					totals[version].AAA.all++;
-					totals[version].AAA[sc.principle.toLowerCase()]++;
-				}
-			}
-		}
-	}
+                if (sc.level === "AA") {
+                    totals[version].AAA.all++;
+                    totals[version].AAA[sc.principle.toLowerCase()]++;
+                }
+            }
+        }
+    }
 
-	return totals;
+    return totals;
 }
