7891010f26786fdce0e2d03cb6b0860497aa7b19
Ned Zimmerman <ned@bight.dev>
2024-07-11 15:59:48 -0300

    fix: resolve linting issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 import { RenderPlugin } from "@11ty/eleventy";
-import syntaxHighlightPlugin from "@11ty/eleventy-plugin-syntaxhighlight";
 import fetch from "@11ty/eleventy-fetch";
+import syntaxHighlightPlugin from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 // Import {$} from 'execa';
 import newIssueUrl from "./src/_utils/new-issue-url.js";
@@ -8,114 +8,102 @@ import scSupport from "./src/_utils/sc-support.js";
 import scTable from "./src/_utils/sc-table.js";
 
 export default function eleventy(eleventyConfig) {
-  eleventyConfig.addNunjucksAsyncShortcode("scTable", scTable);
-  eleventyConfig.addNunjucksAsyncShortcode("scSupport", scSupport);
-  eleventyConfig.addShortcode("newIssueUrl", newIssueUrl);
-  eleventyConfig.addLayoutAlias("report", "report.njk");
-  eleventyConfig.addPlugin(pluginWebc);
+    eleventyConfig.addNunjucksAsyncShortcode("scTable", scTable);
+    eleventyConfig.addNunjucksAsyncShortcode("scSupport", scSupport);
+    eleventyConfig.addShortcode("newIssueUrl", newIssueUrl);
+    eleventyConfig.addLayoutAlias("report", "report.njk");
+    eleventyConfig.addPlugin(pluginWebc);
 
-  eleventyConfig.addGlobalData("successCriteria", async () => {
-    const url =
-      "https://raw.githubusercontent.com/w3c/wcag/main/guidelines/wcag.json";
+    eleventyConfig.addGlobalData("successCriteria", async () => {
+        const url = "https://raw.githubusercontent.com/w3c/wcag/main/guidelines/wcag.json";
 
-    try {
-      const json = await fetch(url, {
-        duration: "1d",
-        type: "json",
-      });
+        try {
+            const json = await fetch(url, {
+                duration: "1d",
+                type: "json"
+            });
 
-      const results = {};
+            const results = {};
 
-      for (const principle of json.principles) {
-        for (const guideline of principle.guidelines) {
-          for (const sc of guideline.successcriteria) {
-            results[sc.num] = {
-              number: sc.num,
-              principle: principle.handle,
-              guideline: guideline.handle,
-              name: sc.handle,
-              level: sc.level,
-              versions: sc.versions,
-              id: sc.id.replace("WCAG2:", ""),
-            };
-          }
+            for (const principle of json.principles) {
+                for (const guideline of principle.guidelines) {
+                    for (const sc of guideline.successcriteria) {
+                        results[sc.num] = {
+                            number: sc.num,
+                            principle: principle.handle,
+                            guideline: guideline.handle,
+                            name: sc.handle,
+                            level: sc.level,
+                            versions: sc.versions,
+                            id: sc.id.replace("WCAG2:", "")
+                        };
+                    }
+                }
+            }
+
+            return results;
+        } catch (error) {
+            console.error(`Fetch failed in successcriteria.js. ${error}`);
         }
-      }
+    });
 
-      return results;
-    } catch (error) {
-      console.error(`Fetch failed in successcriteria.js. ${error}`);
-    }
-  });
+    eleventyConfig.addPlugin(RenderPlugin);
+    eleventyConfig.addPlugin(syntaxHighlightPlugin);
 
-  eleventyConfig.addPlugin(RenderPlugin);
-  eleventyConfig.addPlugin(syntaxHighlightPlugin);
+    eleventyConfig.addFilter("withoutTips", (issues) => issues.filter((item) => Object.hasOwn(item, "sc") && item.sc !== ""));
 
-  eleventyConfig.addFilter("withoutTips", (issues) =>
-    issues.filter((item) => Object.hasOwn(item, "sc") && item.sc !== ""),
-  );
+    eleventyConfig.addFilter("withoutViolations", (issues) => issues.filter((item) => item.sc === "" || !Object.hasOwn(item, "sc")));
 
-  eleventyConfig.addFilter("withoutViolations", (issues) =>
-    issues.filter((item) => item.sc === "" || !Object.hasOwn(item, "sc")),
-  );
+    eleventyConfig.addShortcode("renderString", async function (content, templateFormat) {
+        const renderManager = new RenderPlugin.RenderManager();
+        const result = await renderManager.compile(content, templateFormat);
+        return result.call(this);
+    });
 
-  eleventyConfig.addShortcode(
-    "renderString",
-    async function (content, templateFormat) {
-      const renderManager = new RenderPlugin.RenderManager();
-      const result = await renderManager.compile(content, templateFormat);
-      return result.call(this);
-    },
-  );
+    eleventyConfig.addAsyncFilter("formatDate", (value) =>
+        new Date(value).toLocaleString("en-CA", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        })
+    );
 
-  eleventyConfig.addAsyncFilter("formatDate", (value) =>
-    new Date(value).toLocaleString("en-CA", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    }),
-  );
+    eleventyConfig.addCollection("reports", (collectionApi) => collectionApi.getFilteredByGlob("src/collections/reports/*.md"));
 
-  eleventyConfig.addCollection("reports", (collectionApi) =>
-    collectionApi.getFilteredByGlob("src/collections/reports/*.md"),
-  );
+    eleventyConfig.addPassthroughCopy({
+        "src/admin/config.yml": "admin/config.yml"
+    });
 
-  eleventyConfig.addPassthroughCopy({
-    "src/admin/config.yml": "admin/config.yml",
-  });
+    eleventyConfig.addPassthroughCopy({
+        "src/assets": "assets"
+    });
 
-  eleventyConfig.addPassthroughCopy({
-    "src/assets": "assets",
-  });
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/a11y-syntax-highlighting/dist/prism/a11y-dark.min.css": "assets/styles/a11y-dark.min.css",
+        "node_modules/@zachleat/table-saw/table-saw.js": "assets/scripts/table-saw.js"
+    });
 
-  eleventyConfig.addPassthroughCopy({
-    "node_modules/a11y-syntax-highlighting/dist/prism/a11y-dark.min.css":
-      "assets/styles/a11y-dark.min.css",
-    "node_modules/@zachleat/table-saw/table-saw.js":
-      "assets/scripts/table-saw.js",
-  });
+    // EleventyConfig.on(
+    //   "eleventy.after",
+    //   async ({ _dir, results, _runMode, _outputMode }) => {
+    //     for (const result of results) {
+    //       if (result.inputPath.startsWith("./src/collections/reports/")) {
+    //         const { stdout } =
+    //           await $`weasyprint --pdf-variant=pdf/ua-1 ${result.outputPath} ./_site${result.url}report.pdf`;
+    //       }
+    //     }
+    //   },
+    // );
 
-  // EleventyConfig.on(
-  //   "eleventy.after",
-  //   async ({ _dir, results, _runMode, _outputMode }) => {
-  //     for (const result of results) {
-  //       if (result.inputPath.startsWith("./src/collections/reports/")) {
-  //         const { stdout } =
-  //           await $`weasyprint --pdf-variant=pdf/ua-1 ${result.outputPath} ./_site${result.url}report.pdf`;
-  //       }
-  //     }
-  //   },
-  // );
-
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      layouts: "_layouts",
-      data: "_data",
-    },
-    templateFormats: ["njk", "md", "css", "png", "jpg", "svg"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-  };
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            layouts: "_layouts",
+            data: "_data"
+        },
+        templateFormats: ["njk", "md", "css", "png", "jpg", "svg"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk"
+    };
 }
--- src/_utils/sc-table.js
+++ src/_utils/sc-table.js
@@ -2,35 +2,22 @@ import totalsByLevel from "../_utils/totals-by-level.js";
 import countSuccessCriterionOnce from "./count-success-criteria-once.js";
 
 export default async function scTable(successCriteria, allIssues, data) {
-  const { targetLevel, targetWcagVersion } = data;
+    const { targetLevel, targetWcagVersion } = data;
 
-  let totalsByLevelData;
+    let totalsByLevelData;
 
-  totalsByLevelData = await totalsByLevel(successCriteria);
+    totalsByLevelData = await totalsByLevel(successCriteria);
 
-  // Use string representation of WCAG version to avoid unwanted conversion from e.g. 2.0 to index 2
-  const totals = totalsByLevelData[targetWcagVersion.toString()][targetLevel];
+    // Use string representation of WCAG version to avoid unwanted conversion from e.g. 2.0 to index 2
+    const totals = totalsByLevelData[targetWcagVersion.toString()][targetLevel];
 
-  const perceivable = allIssues
-    .filter((issue) => issue.sc.startsWith("1."))
-    .reduce(countSuccessCriterionOnce, []);
-  const operable = allIssues
-    .filter((issue) => issue.sc.startsWith("2."))
-    .reduce(countSuccessCriterionOnce, []);
-  const understandable = allIssues
-    .filter((issue) => issue.sc.startsWith("3."))
-    .reduce(countSuccessCriterionOnce, []);
-  const robust = allIssues
-    .filter((issue) => issue.sc.startsWith("4."))
-    .reduce(countSuccessCriterionOnce, []);
+    const perceivable = allIssues.filter((issue) => issue.sc.startsWith("1.")).reduce(countSuccessCriterionOnce, []);
+    const operable = allIssues.filter((issue) => issue.sc.startsWith("2.")).reduce(countSuccessCriterionOnce, []);
+    const understandable = allIssues.filter((issue) => issue.sc.startsWith("3.")).reduce(countSuccessCriterionOnce, []);
+    const robust = allIssues.filter((issue) => issue.sc.startsWith("4.")).reduce(countSuccessCriterionOnce, []);
 
-  const totalConforming =
-    totals.perceivable -
-    perceivable.length +
-    (totals.operable - operable.length) +
-    (totals.understandable - understandable.length) +
-    (totals.robust - robust.length);
-  return `
+    const totalConforming = totals.perceivable - perceivable.length + (totals.operable - operable.length) + (totals.understandable - understandable.length) + (totals.robust - robust.length);
+    return `
   <table class="sc-table">
   <thead>
     <tr>
