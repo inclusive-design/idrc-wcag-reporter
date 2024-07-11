45fe917e88f33665c4a10b121f823b09ea95765f
Ned Zimmerman <ned@bight.dev>
2024-07-11 16:48:46 -0300

    feat: render in templates

--- src/_utils/sc-table.js
+++ src/_utils/sc-table.js
@@ -17,35 +17,27 @@ export default async function scTable(successCriteria, allIssues, data) {
     const robust = allIssues.filter((issue) => issue.sc.startsWith("4.")).reduce(countSuccessCriterionOnce, []);
 
     const totalConforming = totals.perceivable - perceivable.length + (totals.operable - operable.length) + (totals.understandable - understandable.length) + (totals.robust - robust.length);
-    return `
-  <table class="sc-table">
-  <thead>
-    <tr>
-      <th>Principle</th>
-      <th>Results by principle</th>
-    </tr>
-  </thead>
-  <tbody>
-  <tr>
-    <td>Perceivable</td>
-    <td>${totals.perceivable - perceivable.length} of ${totals.perceivable}</td>
-  </tr>
-  <tr>
-      <td>Operable</td>
-      <td>${totals.operable - operable.length} of ${totals.operable} </td>
-  </tr>
-  <tr>
-      <td>Understandable</td>
-      <td>${totals.understandable - understandable.length} of ${totals.understandable}</td>
-  </tr>
-  <tr>
-      <td>Robust</td>
-      <td>${totals.robust - robust.length} of ${totals.robust} </td>
-  </tr>
-  <tr>
-      <td>Total</td>
-      <td>${totalConforming} of ${totals.all}</td>
-  </tr>
-  <tbody>
-  </table>`;
+
+    return {
+        Perceivable: {
+            supported: totals.perceivable - perceivable.length,
+            total: totals.perceivable
+        },
+        Operable: {
+            supported: totals.operable - operable.length,
+            total: totals.operable
+        },
+        Understandable: {
+            supported: totals.understandable - understandable.length,
+            total: totals.understandable
+        },
+        Robust: {
+            supported: totals.robust - robust.length,
+            total: totals.robust
+        },
+        Total: {
+            supported: totalConforming,
+            total: totals.all
+        }
+    };
 }
