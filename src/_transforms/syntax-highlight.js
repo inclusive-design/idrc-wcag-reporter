1ef22ddb67c93b867d072f44e1d7259a0b1568cd
Ned Zimmerman <ned@bight.dev>
2024-07-23 10:24:04 -0300

    feat: configure Light Owl and Night Owl for code

--- src/_transforms/syntax-highlight.js
+++ src/_transforms/syntax-highlight.js
@@ -1,17 +1,32 @@
+import { readFile } from "fs/promises";
 import { parseHTML } from "linkedom";
-import { codeToHtml } from "shiki";
+import { bundledLanguages, createHighlighter } from "shiki";
 
 export default async function (content) {
     if (this.page.outputPath && this.page.outputPath.includes(".html")) {
         const { document } = parseHTML(content);
         const codeBlocks = [...document.querySelectorAll("pre code")];
 
+        const lightOwl = JSON.parse(await readFile(new URL("./../../node_modules/night-owl/themes/Night Owl-Light-color-theme-noitalic.json", import.meta.url)));
+
+        lightOwl.name = "light-owl";
+
+        const highlighter = await createHighlighter({
+            themes: [lightOwl, "night-owl"],
+            langs: Object.keys(bundledLanguages)
+        });
+
         if (codeBlocks.length > 0) {
             for (const codeBlock of codeBlocks) {
                 const lang = codeBlock.className.replace("language-", "");
-                const html = await codeToHtml(codeBlock.textContent, {
+
+                const html = highlighter.codeToHtml(codeBlock.textContent, {
                     lang,
-                    theme: "night-owl"
+                    themes: {
+                        dark: "night-owl",
+                        light: "light-owl"
+                    },
+                    defaultColor: false
                 });
 
                 codeBlock.parentNode.outerHTML = html;
